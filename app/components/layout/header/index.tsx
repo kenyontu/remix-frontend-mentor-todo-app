3e5653b62fdaf784f12905b505e7741f584c567e
kenyontu <kenyontu@outlook.com>
2022-06-24 23:29:29 +0800

    add spinner to indicate ongoing actions

--- app/components/layout/header/index.tsx
+++ app/components/layout/header/index.tsx
@@ -1,11 +1,13 @@
 import type { LinksFunction } from '@remix-run/node'
 import { ClientOnly } from 'remix-utils'
+import cx from 'classnames'
 
 import styles from './styles.css'
 import {
   ThemeToggle,
   links as themeToggleLinks,
 } from '~/components/theme-toggle'
+import { useFetchers } from '@remix-run/react'
 
 export const links: LinksFunction = () => [
   ...themeToggleLinks(),
@@ -13,12 +15,24 @@ export const links: LinksFunction = () => [
 ]
 
 function Header() {
+  const hasOngoingAction = useHasOngoingAction()
+
   return (
     <header className="header">
-      <h1 className="title">Todo</h1>
+      <h1 className={cx('title', { loading: hasOngoingAction })}>Todo</h1>
       <ClientOnly>{() => <ThemeToggle />}</ClientOnly>
     </header>
   )
+}
+
+function useHasOngoingAction() {
+  const fetchers = useFetchers()
+
+  for (let i = 0; i < fetchers.length; i++) {
+    if (fetchers[i].submission) return true
+  }
+
+  return false
 }
 
 export { Header }
