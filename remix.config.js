fa53dd4c67d692c8a18deea668d39d22bca29ba2
kenyontu <kenyontu@outlook.com>
2022-06-26 13:52:24 +0800

    fix: devServerPort needed to make live-reload work

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,7 @@ module.exports = {
   serverBuildTarget: 'netlify',
   server: './server.js',
   ignoredRouteFiles: ['**/.*'],
-  devServerPort: 8002,
+  // devServerPort: 8002,
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: ".netlify/functions-internal/server.js",
